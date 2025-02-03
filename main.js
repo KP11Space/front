3eccc20ec2ca3551bfa7ab3192d3bb02234d2ac8
EroshinaV <VeraEro@yandex.ru>
2025-02-03 12:07:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+document.addEventlistener('DOMContentLoaded', function()
+{         
+     const imgTaskAdd = document.getElementById("img-post-task")
+     imgTaskAdd.addEventlistener('click', function(){
+        createTask
+     })
+     getTasks()
+     getTasks()
+     //getTasks()
+     //createtask("test", date.toISOString())
+}, false)
+
+const mockapiURL = "https://678d26c2f067bf9e24e97242.mockapi.io/task"
+function getTasks(){
+    fetch(mockapiURL, {
+        method: 'GET',
+        headers: {'content - type':'application/json'},
+    }).then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+    }).then(tasks => {
+        conatainer.innerHTML = ''
+        tasks.forEach(task => {
+           createElementAndApppendChild(task["name"])   
+            
+        });
+        
+    }).catch(error => {
+
+    })
+}
+function createTask(){
+    const inputTask = document.getElementById("input-post-task")
+    var date = new Date();
+    const newTask = {
+        name: inputTask.value,
+        is_complated: false,
+        created: date.toISOString()
+      };
+      
+      fetch(mockapiURL, { method: 'POST',
+        headers: {'content-type':'application/json'},
+        body: JSON.stringify(newTask)
+      }).then(res => {
+        inputTask.value = ""
+        if (res.ok) {
+            return res.json();
+        }
+      }).then(task => {
+        inputTask.value =  ""
+        createElementAndApppendChild(task["name"]) 
+      }).catch(error => {
+        
+      })
+}
+function createElementAndApppendChild(taskName){
+  const conatainer = document.getElentyById("task-container")
+  const li = document.createElement("li")
+   li.classlist += "task-item"
+       li.innerHTML =  `<span>${taskName}</span>
+         <div class="flex gap-3">
+            <img class="cursor-pointer duration-300 hover:scale-110" src=".Done.svg" alt="" srcset=""> 
+              <img class="cursor-pointer duration-300 hover:scale-110" src=".Edit.svg" alt="" srcset="">
+               <img class="cursor-pointer duration-300 hover:scale-110" src=".Trash.svg" alt="" srcset="">
+          </div>`
+   container.appendChild(li)
+
+}
+
+
+
